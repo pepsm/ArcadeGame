01034b802575c81f5f5341057aec6e661dafd9c4
pepsm <peps.mihailov@gmail.com>
2018-05-28 02:14:50 +0300

    upd

---
+++ Scripts/GunShot.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunShot : MonoBehaviour {
+
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			GetComponent<Animation>().Play("GunFire");
+		}	
+	}
+}
