9444d19884742871c7a5a04b6836e4bdbb1752be
pepsm <peps.mihailov@gmail.com>
2018-05-28 11:34:19 +0300

    update

---
+++ Scripts/AmmoPick.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPick : MonoBehaviour {
+
+	public AudioSource gunsound;
+	public int AmmoAmount = 10;
+	void OnTriggerEnter(Collider col){
+		
+		gunsound.Play();
+
+		GlobalAmmo.CurrentAmmo += AmmoAmount;
+		this.gameObject.SetActive (false);
+	}
+}
