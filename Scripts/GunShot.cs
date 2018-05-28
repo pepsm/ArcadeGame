c804a7725b7623d30ff891aba555b53cc10bffc6
pepsm <peps.mihailov@gmail.com>
2018-05-28 11:35:53 +0300

    upd

--- Scripts/GunShot.cs
+++ Scripts/GunShot.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class GunShot : MonoBehaviour {
-
 	void Update () {
 		if (Input.GetButtonDown ("Fire1")) {
+		    AudioSource gunsound = GetComponent<AudioSource>();
+			gunsound.Play();
 			GetComponent<Animation>().Play("GunFire");
+			GlobalAmmo.CurrentAmmo -= 1;
 		}	
 	}
 }
