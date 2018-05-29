6a10fe43ac9139f657f2631da0703bde40c914df
pepsm <peps.mihailov@gmail.com>
2018-05-30 00:06:15 +0300

    update

---
+++ Scripts/OpenDoor001.js
@@ -0,0 +1,39 @@
+﻿import UnityEngine.UI;
+
+var TextDisplay : GameObject;
+var TheDistance : float = PlayerCasting.DistanceFromTarget;
+
+var TheDoor : GameObject;
+
+
+
+function Update () {
+	TheDistance = PlayerCasting.DistanceFromTarget;		
+}
+
+function OnMouseOver () {
+	if (TheDistance <= 2) {
+		TextDisplay.GetComponent.<Text>().text = "Press 'e' button";
+	}
+
+	if (Input.GetButtonDown("Action")) {
+		if (TheDistance <= 2) {
+			OpenTheDoor();
+
+		}
+	}
+}
+
+function OnMouseExit () {
+	TextDisplay.GetComponent.<Text>().text = "";
+}
+
+function OpenTheDoor () {
+	TheDoor.GetComponent("Animator").enabled=true;
+	yield WaitForSeconds(1);
+	TheDoor.GetComponent("Animator").enabled=false;
+	yield WaitForSeconds(5);
+	TheDoor.GetComponent("Animator").enabled=true;
+	yield WaitForSeconds(1);
+	TheDoor.GetComponent("Animator").enabled=false;
+}
