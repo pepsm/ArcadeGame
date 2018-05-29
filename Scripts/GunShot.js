416bb19edcc99a854f8e26b06ded8d914e8b3eae
Constantin Mihaylov <24604241+TheMustardVader@users.noreply.github.com>
2018-05-29 11:28:51 +0300

    javascript koki

---
+++ Scripts/GunShot.js
@@ -0,0 +1,21 @@
+﻿var Flash : GameObject;
+function Update () { 
+	
+	if(GlobalAmmo.LoadedAmmo >= 1){
+		//Edit/ProjectSettings/Input
+		if(Input.GetButtonDown("Fire1")) {
+			var gunsound : AudioSource = GetComponent.<AudioSource>();
+			gunsound.Play();
+			Flash.SetActive(true);
+			MuzzleOff();
+			GetComponent.<Animation>().Play("GunShot");
+			GlobalAmmo.LoadedAmmo -= 1;
+		}
+	}
+
+}
+
+function MuzzleOff() {
+	 yield WaitForSeconds(0.1);
+	 Flash.SetActive(false);
+}
---
+++ Scripts/OpenDoor001.js
@@ -0,0 +1,39 @@
+﻿import UnityEngine.UI;
+
+var TextDisplay : GameObject;
+var TheDistance : float = PlayerCasting.DistanceFromTarget;
+
+var TheDoor : GameObject;
+
+
+
+function Update () {
+	TheDistance = PlayerCasting.DistanceFromTarget;
+	if (Input.GetButtonDown("Action")) {
+		if (TheDistance <= 2) {
+			OpenTheDoor();
+
+		}
+	}
+		
+}
+
+function OnMouseOver () {
+	if (TheDistance <= 2) {
+		TextDisplay.GetComponent.<Text>().text = "Press 'e' button";
+	}
+}
+
+function OnMouseExit () {
+	TextDisplay.GetComponent.<Text>().text = "";
+}
+
+function OpenTheDoor () {
+	TheDoor.GetComponent("Animator").enabled=true;
+	yield WaitForSeconds(1);
+	TheDoor.GetComponent("Animator").enabled=false;
+	yield WaitForSeconds(5);
+	TheDoor.GetComponent("Animator").enabled=true;
+	yield WaitForSeconds(1);
+	TheDoor.GetComponent("Animator").enabled=false;
+}
